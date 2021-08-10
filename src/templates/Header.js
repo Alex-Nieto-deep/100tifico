d17e97fcafdacf36e7936aed7a9242a714e00f03
Luis Alejandro <luisalejandronietoruth@gmail.com>
2021-08-10 16:27:40 -0500

    Crear rutas del sitio

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,21 +1,21 @@
 const Header = () => {
   const view = `
-  <div class="Header-main">
-    <div class="Header-logo">
-      <h1>
-        <a href="/">
-          100tifi.co
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="/">
+            100tifi.co
+          </a>
+        </h1>
+      </div>
+      <div class="Header-nav">
+        <a href="#/about/">
+          About
         </a>
-      </h1>
+      </div>
     </div>
-    <div class="Header-nav">
-      <a href="#/about">
-        about
-      </a>
-    </div>
-  </div>
-  return view;
   `;
-}
+  return view;
+};
 
 export default Header;
