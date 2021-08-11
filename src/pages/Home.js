491dfebfc823bdbbdf138440eca669f4209ed083
Luis Alejandro <luisalejandronietoruth@gmail.com>
2021-08-11 08:13:02 -0500

    Obtener personajes con la función de llamado a la API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
   const view = `
     <div class="Characters">
-      <article class="Characters-item">
-        <a href="#/1/">
-          <img src="image" alt="name">
-          <h2>Name</h2>
-        </a>
-      </article>
+      ${characters.results.map(character => `
+        <article class="Characters-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+      `).join('')}
     </div>
   `;
   return view;
---
+++ src/utils/getData.js
@@ -0,0 +1,15 @@
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const getData = async (id) => {
+  const apiURL = id ? `${API}${id}` : API;
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    return data;
+  }
+  catch (err) {
+    console.log('fetch Error', err);
+  }
+}
+
+export default getData;
